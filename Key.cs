af670bf263d17325aeb5e2daa7ec4386ae1b71fa
Aotmick <71666540+Aotmick@users.noreply.github.com>
2021-12-30 21:05:22 +0600

    180151

---
+++ Key.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Inventory_Mangaement
+{
+    public partial class Key : Form
+    {
+        String cs = ConfigurationManager.ConnectionStrings["Inventory_Mangaement.Properties.Settings.inventory_managementdbConnectionString"].ConnectionString;
+        public Key()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string query = "Insert into Keys (RoomNo,Status) values ('" + textBox5.Text + "',' Available')";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            textBox5.Text = "";
+            MessageBox.Show("Key Added Successfully");
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            String query = "Select * From Keys";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            SDA.Fill(dt);
+            dataGridView2.DataSource = dt;
+            con.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            String query = "Delete From Keys Where RoomNo ='" + textBox5.Text + "'";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            textBox5.Text = "";
+            MessageBox.Show("Key Deleted Successfully");
+        }
+
+        private void dataGridView2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox5.Text = dataGridView2.SelectedRows[0].Cells[1].Value.ToString();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string query = "Update Key_Issue set ReturnTime='"+textBox4.Text+"' where RoomNo ='"+textBox1.Text+"'";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+
+            String query2 = "Update Keys Set Status ='Available' where RoomNo ='" + textBox1.Text + "'";
+            SqlDataAdapter SDB = new SqlDataAdapter(query2, con);
+            SDB.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            MessageBox.Show("Key Returned Successfully");
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            String query = "Delete From Key_Issue Where RoomNo ='" + textBox1.Text + "'";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            MessageBox.Show("Record Deleted Successfully");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            String query = "Select * From Key_Issue";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            SDA.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            textBox4.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            String query = "Update Key_Issue SET PersonID = '" + textBox2.Text + "',IssueTime ='" + textBox3.Text + "' ,ReturnTime = '" + textBox4.Text + "' Where RoomNo ='" + textBox1.Text + "'";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            MessageBox.Show("Data updated successfully");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string query = "Insert into Key_Issue (RoomNo,PersonID,IssueTime) values ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            SqlDataAdapter SDA = new SqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+
+            String query2 = "Update Keys Set Status ='Occupied' where RoomNo ='" + textBox1.Text + "'";
+            SqlDataAdapter SDB = new SqlDataAdapter(query2, con);
+            SDB.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            MessageBox.Show("Key Issued Successfully");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Home home = new Home();
+            home.Show();
+        }
+    }
+}
